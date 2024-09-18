d4ce3ab6a730916c5edc5633dd28a856faa85c5f
Sakthi544 <rsakthiparkavi@gmail.com>
2024-09-18 19:29:15 +0530

    Add files via upload

---
+++ nice.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Bread from '../component5/Bread1';
+import Address from '../component5/Address2';
+import Wallet from '../component5/Wallet3';
+import Order from '../component5/Order4';
+import Wishlist from '../component5/Wishlist5';
+import Payment from '../component5/Payment6';
+import Footer from '../component5/Footer7';
+function App() {
+  return(
+     <div>
+        <Bread/>
+        <Address/>
+        <Wallet/>
+        <Order/>
+        <Wishlist/>
+        <Payment/>
+        <Footer/>
+    </div>
+  );
+}
+export default App;
---
+++ signup.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import Signup from '../component4/signup';
+function App()  
+{
+    return(
+        <div>
+            <Signup/>
+        </div>
+    );
+}
+export default App;
