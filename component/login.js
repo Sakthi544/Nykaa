b9df030dfe053909503304eb9de2725c01778c2f
Sakthi544 <rsakthiparkavi@gmail.com>
2024-09-18 19:56:03 +0530

    Add files via upload

---
+++ component/login.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Login from '../component3/login';
+function App() 
+{
+    return(
+      <div>
+         <Login/>
+      </div>
+       
+    );
+   
+
+}
+export default App;
